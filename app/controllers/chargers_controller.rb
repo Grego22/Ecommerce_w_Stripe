6875900baffc69c1b324b7e83ff84934a7e446a2
Greg Garville Jr <gjgarvil@gmail.com>
2017-10-20 12:26:02 -0400

    coded chargers controller and added purchases views to show confirmation page upon purchase

--- app/controllers/chargers_controller.rb
+++ app/controllers/chargers_controller.rb
@@ -14,6 +14,11 @@ class ChargersController < ApplicationController
       :currency    => 'usd'
     )
 
+purchase =Purchase.create(email:params[:stripeEmail], card:params[:stripeToken],
+amount: params[:amount], description: charge.description, currency: charge.currency,
+customer_id: customer_id, product_id: 1)
+
+redirect_to purchase
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
